c63f9af0b777cdd1dcd565001c03cc06e336d26d
Ryan McLaughlin <ryanjmclaughlin@gmail.com>
2011-01-04 19:35:30 -0500

    Updated rails to 3.0.3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TrainingTracker::Application.config.secret_token = '7e93a9c95ea97307f97c19822ebacdb5326043f3c619eb8161a59d0a618ad3b6d652cc99b9f564dc661edd39a721e565c298b6408d8bf610c44767808e0238cb'
